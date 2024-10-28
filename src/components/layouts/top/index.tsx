bb99cacb2f4dc85422e4fe2157487099a0767196
Kazuki_Jo <kazuki.castle0418@gmail.com>
2024-10-27 23:49:27 -0700

    fix video

--- src/components/layouts/top/index.tsx
+++ src/components/layouts/top/index.tsx
@@ -4,12 +4,12 @@ export const Top = () => {
   return (
     <div className="h-screen w-screen" id="top">
       <video
-        aria-disabled
         src={require("/public/top.mov")}
         autoPlay
         muted
         loop
-        className="w-full h-full object-cover absolute top-0 left-0 "
+        controls={false}
+        className="w-full h-full object-cover absolute top-0 left-0"
       />
       <TopPage />
     </div>
